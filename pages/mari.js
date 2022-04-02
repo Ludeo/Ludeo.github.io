62869650f46e0c0273c1f97f72a5277b824ec388
Ludeo <lucas.steuer1@gmx.de>
2022-04-02 07:17:27 +0200

    trying out next.js

---
+++ pages/mari.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function Mari() {
+  return (
+    <div>
+      <Head>
+        <title>Mari Shop</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <div>Mari Shop</div>
+    </div>
+  );
+}
---
+++ pages/tradeitemexchange.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function TradeItemExchange() {
+  return (
+    <div>
+      <Head>
+        <title>Trade Item Exchange</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <div>Trade Item Exchange</div>
+    </div>
+  );
+}
