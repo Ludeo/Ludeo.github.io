91434e09cf0a1075d62203eb1ed1183e40e91fee
Ludeo <lucas.steuer1@gmx.de>
2022-04-03 21:18:50 +0200

    changed something v3

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  reactStrictMode: true,
-  assetPrefix: isProd ? '/intralismlostark/' : ''
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
 }
