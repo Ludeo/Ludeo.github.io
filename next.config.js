592a6d32b96bb819d6291bef6a2325dcabcbb789
Ludeo <lucas.steuer1@gmx.de>
2022-04-03 20:51:34 +0200

    ssss

--- next.config.js
+++ next.config.js
@@ -3,5 +3,5 @@ const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
   reactStrictMode: true,
-  assetPrefix: isProd ? '/intralismlostark/out/' : ''
+  assetPrefix: isProd ? '/intralismlostark/' : ''
 }
