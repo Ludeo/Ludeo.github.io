1a45fdfecfe05e0c0d609d49c7107da116d50e5e
Ludeo <lucas.steuer1@gmx.de>
2022-04-03 21:20:35 +0200

    ssssssss

--- pages/mari.js
+++ pages/mari.js
@@ -10,7 +10,7 @@ export default function Mari() {
   return (
     <div>
       <Head>
-        <title>Mari Shop</title>
+        <title>Mari Shop </title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <div>
