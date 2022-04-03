eded1933345e6701715760244dc00f9386e065a0
Ludeo <lucas.steuer1@gmx.de>
2022-04-03 20:40:44 +0200

    trying to get it to work

--- next.config.js
+++ next.config.js
@@ -3,5 +3,5 @@ const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
   reactStrictMode: true,
-  assetPrefix: isProd ? '/intralismlostark/' : ''
+  assetPrefix: isProd ? '/intralismlostark/out/' : ''
 }
