d2ece3e31bf34b7dac96a9252c681b705392af3b
Ludeo <lucas.steuer1@gmx.de>
2022-04-03 20:20:33 +0200

    changed next config

--- next.config.js
+++ next.config.js
@@ -3,5 +3,5 @@ const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
   reactStrictMode: true,
-  assetPrefix: isProd ? '/ludeo.github.io/' : ''
+  assetPrefix: isProd ? '/intralism-lost-ark/' : ''
 }
